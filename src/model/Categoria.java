5a424cca39681f73ebb1f021f4e80a87ab1551b8
robertaalcantara <roberta_alcantara@outlook.com.br>
2021-09-16 16:30:06 -0300

    Initial commit

---
+++ src/model/Categoria.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Categoria {
+    int codCategoria;
+    String nome;
+
+    public Categoria(int codCategoria, String nome) {
+        this.codCategoria = codCategoria;
+        this.nome = nome;
+    }
+
+    public Categoria(){
+
+    }
+
+    public int getCodCategoria() {
+        return codCategoria;
+    }
+
+    public void setCodCategoria(int codCategoria) {
+        this.codCategoria = codCategoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
