23a3dad6e6ce14e83809fc752ea3e2f3bc43cec5
robertaalcantara <roberta_alcantara@outlook.com.br>
2021-09-19 20:21:07 -0300

    dao item pedido delivery

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -10,33 +10,23 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws ParseException {
 
-       ArrayList<PedidoFormaPagamento> lista = null;
-       PedidoFormaPagamento pedidoFormaPagamento = new PedidoFormaPagamento();
-       DeliveryFormaPagamentoDao daoDeliveryFormaPagamento = new DeliveryFormaPagamentoDao();
+       ArrayList<ItemPedido> lista = null;
+       ItemPedido itemPedido = new ItemPedido();
+       DeliveryItemPedidoDao daoDeliveryItemPedido = new DeliveryItemPedidoDao();
 
        int cod;
        String nome;
 
-        String dataString = "03/02/2021";
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        java.sql.Date data = new java.sql.Date(format.parse(dataString).getTime());
+        itemPedido.setCodItem(2);
+        itemPedido.setCodPedido(23);
 
-        pedidoFormaPagamento.setCodFormaPagamento(2);
-        pedidoFormaPagamento.setFinalizado(false);
-        pedidoFormaPagamento.setCodPedido(31);
-        pedidoFormaPagamento.setDataCaixa(data);
-        pedidoFormaPagamento.setValor(22.00);
-
-
-        lista = daoDeliveryFormaPagamento.listarPedidoFormaPagamentoPorStatus(true);
+        //daoDeliveryItemPedido.excluirItemPedido(itemPedido);
+        lista = daoDeliveryItemPedido.listarItensPorPedido(itemPedido.getCodPedido());
 
         for (int i = 0; i < lista.size(); i++) {
             System.out.println(
-                    lista.get(i).getCodFormaPagamento() + ", " +
-                    lista.get(i).getCodPedido() + ", " +
-                     lista.get(i).isFinalizado() + ", " +
-                   lista.get(i).getDataCaixa() + ", " +
-                    lista.get(i).getValor() + ". \n ");
+                    lista.get(i).getCodItem() + ", " +
+                    lista.get(i).getCodPedido() + ", " + ". \n ");
         }
     }
 }
