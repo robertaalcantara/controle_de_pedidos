1c2e2292d3577f40fe71a2508e940bc009fb8152
robertaalcantara <roberta_alcantara@outlook.com.br>
2021-09-20 00:01:45 -0300

    criar categoria

--- src/view/AdicionarCategoria.java
+++ src/view/AdicionarCategoria.java
@@ -1,5 +1,8 @@
 package view;
 
+import dao.CategoriaDao;
+import model.Categoria;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,6 +30,12 @@ public class AdicionarCategoria extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 //update tabela categoria
+                CategoriaDao daoCategoria = new CategoriaDao();
+                Categoria categoria = new Categoria();
+
+                categoria.setNome(nomeCategoriaTextField.getText());
+                daoCategoria.inserirCategoria(categoria);
+
                 Produtos produtos = new Produtos("Produtos");
                 produtos.setVisible(true);
                 dispose();
