ce55e9f05addafbc1d71f2ea59f9b1031bc419fc
EnergyFall266 <leonardo.vanzin@hotmail.com>
2021-09-19 22:42:38 -0300

    balcao

--- src/view/Pedidos.java
+++ src/view/Pedidos.java
@@ -69,8 +69,8 @@ public class Pedidos extends JFrame {
         balcaoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                SelecaoClienteBalcao selecaoClienteBalcao = new SelecaoClienteBalcao("Balcao");
-                selecaoClienteBalcao.setVisible(true);
+                Balcao balcao = new Balcao("Balcao");
+                balcao.setVisible(true);
                 dispose();
             }
         });
